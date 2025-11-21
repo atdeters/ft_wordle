b1f90c44a1510e649996573d7a208aa90cff71e3
atdeters <sreted@outlook.com>
2025-11-21 22:22:04 +0100

    chore: Delete executables and add .gitignore for the future

--- target/debug/deps/wordle-15202ee7f483f4f5.d
+++
@@ -1,5 +0,0 @@
-/sgoinfre/adeters/ft_wordle/target/debug/deps/wordle-15202ee7f483f4f5.d: src/main.rs
-
-/sgoinfre/adeters/ft_wordle/target/debug/deps/wordle-15202ee7f483f4f5: src/main.rs
-
-src/main.rs:
--- target/debug/wordle.d
+++
@@ -1,1 +0,0 @@
-/sgoinfre/adeters/ft_wordle/target/debug/wordle: /sgoinfre/adeters/ft_wordle/src/main.rs
