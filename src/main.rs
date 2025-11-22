d2caa8a32d822eccb05dbe4aa549788318a7677d
atdeters <sreted@outlook.com>
2025-11-22 15:30:47 +0100

    feat: Add logic for filling the buffer
    Game is pretty much playable now, eventhough it is not very
    pretty

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-// use std::collections::HashSet;
 use std::collections::HashSet;
 use colored::Colorize;
+use console::Term;
 
 fn main() {
 	// Get binary and store the contents from the text file into a string
@@ -20,26 +20,79 @@ fn main() {
         char_counter_wtf[char as usize - 'a' as usize] += 1;
     }
 
-    buffer[1][0] = 'a';
-    buffer[1][1] = 'w';
-    buffer[1][2] = 'a';
-    buffer[1][3] = 'r';
-    buffer[1][4] = 'd';
-
     let mut char_nb: usize;
-    for elem in buffer {
-        // Check if elem is part of the wordlist
-        let current_word: String = elem.iter().collect();
-        let curr: &str = current_word.as_str();
-        if !dict.contains(curr) {
-            println!("Word not in wordlist: {current_word}");
-            continue;
+    let term = Term::stdout(); // Terminal used to read input from user
+    for i in 0..6 {
+
+        // Let user build the word in the buffer
+        let mut idx: usize = 0;
+        let mut key_in: console::Key;
+        let mut char_in: char = '_';
+        loop {
+            while char_in != '\n' {
+                // Get a key
+                key_in = term
+                            .read_key()
+                            .expect("Reason"); // TODO: Read what this is all about
+               
+                
+                // Turn key into char
+                match key_in {
+                    console::Key::Char(c) => {
+                        char_in = c;
+                    }
+                    console::Key::Backspace => {
+                        char_in = 127 as char;
+                    }
+                    console::Key::Enter => {
+                        char_in = '\n';
+                    }
+                    _ => {
+                        char_in = '_';
+                    }
+                }
+
+                // println!("Latest char input: {}", key_in);
+                if char_in == 127 as char && idx > 0 {
+                    idx -= 1;
+                    buffer[i][idx] = '_';
+                }
+                else if idx <= 4 && char_in.is_alphabetic() {
+                    buffer[i][idx] = char_in;
+                    idx += 1;
+                }
+                /*
+                let tmp_word: String = elem.iter().collect();
+                let current_word: &str = tmp_word.as_str();
+                println!("{current_word}");
+                */
+                print!("\x1B[2J\x1B[H");
+                for line in buffer {
+                    println!("{:?}", line);
+                }
+            }
+
+            // Create a string out of our current buffer
+            let tmp_word: String = buffer[i].iter().collect();
+            let current_word: &str = tmp_word.as_str();
+
+            if idx != 5 {
+                println!("Not enough letters");
+            }
+            else if !dict.contains(current_word) {
+                println!("Word not in wordlist: {current_word}");
+            }
+            else {
+                break;
+            }
+            char_in = '_';
         }
 
-        // Reveal last word
+        // Reveal information about latest word
         char_nb = 0;
         let mut char_counter_curr = char_counter_wtf;
-        for character in elem {
+        let mut correct_chars: u8 = 0;
+        for character in buffer[i] {
             // Character in right position
             if word_to_find.chars().nth(char_nb) == Some(character) {
                 print!("{}", character
@@ -47,9 +100,10 @@ fn main() {
                                 .bold()
                                 .black()
                                 .on_truecolor(128, 239, 128));
+                char_counter_curr[character as usize - 'a' as usize] -= 1;
+                correct_chars += 1;
             }
             // Character in word but wrong position
-            // TODO: Make this work with char_coutner_curr
             else if char_counter_curr[character as usize - 'a' as usize] != 0 {
                 print!("{}", character
                                 .to_string()
@@ -64,7 +118,11 @@ fn main() {
             print!(" ");
             char_nb += 1;
         }
+        if correct_chars == 5 {
+            println!("Won game");
+            std::process::exit(0);
+        }
         println!("");
     }
-    println!("The word was \"{}\"", word_to_find);
+    println!("You lose. The word was \"{}\"", word_to_find);
 }
