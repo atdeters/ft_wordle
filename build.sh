4fc11c5bd51ac7581358caecb5c2dbf8975e1609
atdeters <sreted@outlook.com>
2025-11-25 19:10:45 +0100

    refactor: Add deploy folder to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 rustup default stable
 rustup target add wasm32-unknown-unknown
 cargo build --release --target wasm32-unknown-unknown
-cp ./target/wasm32-unknown-unknown/release/wordle.wasm ./
+mkdir -p deploy
+cp ./target/wasm32-unknown-unknown/release/wordle.wasm ./deploy
+cp index.html ./deploy
 
